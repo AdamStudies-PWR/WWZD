8b4f806fc041d6118eef4acf2d899e8434dd6921
Adam Krizar <adam.krizar@gmail.com>
2023-11-17 17:24:16 +0100

    Create base rest server

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
-from utils.file_utils import
+from utils.file_utils import get_data, list_data
 
 
 app = Flask("news-room-api")
 
-@app.route('/data', methods = ['GET'])
-def data_list():
-    pass
+
+@app.route('/list_data', methods = ['GET'])
+def list_data_():
+    return list_data()
+
+
+@app.route('/get_data', methods = ['GET'])
+def get_data_():
+    j_request = request.get_json()
+    return get_data(j_request)
+
+if __name__ == "__main__":
+    app.run()
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -5,6 +5,29 @@ import json
 DATA_PATH = "dataframe"
 
 
-def get_data_list():
+def list_data():
+    data = {}
     for filename in os.listdir(DATA_PATH):
-        #TODO finish
+        data_descriptor = filename.split(".")[0]
+        split_descriptor = data_descriptor.split("_")
+        dataset = split_descriptor[0]
+        model = split_descriptor[1]
+        data[dataset] = data.get(dataset, []) + [model]
+
+    return json.dumps(data)
+
+
+def get_data(request):
+    if  not "dataset" in request or not "model" in request:
+        return "Missing data", 400
+
+    dataset = request["dataset"]
+    model = request["model"]
+    path = os.path.join(DATA_PATH, dataset + "_" + model + ".json")
+
+    if not os.path.exists(path):
+        print(path)
+        return "Json file for reqested dataset and model not found", 404
+
+    file = open(path)
+    return json.load(file)
