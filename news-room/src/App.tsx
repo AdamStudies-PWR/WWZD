c76615a70864679f17d5a0f1cd0c2e995a58bb22
Adam Krizar <adam.krizar@gmail.com>
2023-11-17 19:34:16 +0100

    Test spinner

--- news-room/src/App.tsx
+++ news-room/src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component
+{
+    readonly SERVER_URI: string = "http://127.0.0.1:5000/";
+    readonly LIST_DATA_REQ: string = "list_data";
+    readonly GET_DATA_REQ: string = "get_data";
+
+    DATASET_MAP = new Map<string, string[]>();
+    DATASETS: string[] = ["test", "test2", "test3"]
+
+    handleGetListResponse(response:any)
+    {
+        this.DATASET_MAP.clear();
+
+        for (let item in response.some)
+        {
+            let key: string = item.toString();
+            let values: string[] = response.some[key];
+            this.DATASET_MAP.set(key, values);
+
+            if (!this.DATASETS.includes(key))
+            {
+                this.DATASETS.push(key)
+            }
+        }
+
+        console.log(this.DATASETS)
+    }
+
+    async getList()
+    {
+        const REQUEST_URI = this.SERVER_URI + this.LIST_DATA_REQ;
+        fetch(REQUEST_URI)
+            .then(response => response.json())
+            .then((data) => this.handleGetListResponse(data))
+            .catch((err) => { console.log(err.message)});
+    }
+
+    render(): React.ReactNode
+    {
+        this.getList();
+        return (
+            <div className='App'>
+                <h1>News Room</h1>
+                <div>
+                    <label>
+                        Dataset: &nbsp;
+                        <select>
+                            {this.DATASETS.map((element) => (
+                                <option value={element}> {element} </option>
+                            ))}
+                        </select>
+                    </label>
+                </div>
+            </div>);
+    }
 }
 
 export default App;
