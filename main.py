d3dcfb146724ee2395f430da16613e50cf5de07e
Adam Krizar <adam.krizar@gmail.com>
2023-11-18 19:08:42 +0100

    Update server tp handle CORS messages

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import json
 
-from flask import Flask, request
+from flask import Flask, request, Response
+from flask_cors import CORS
 
 from utils.file_utils import get_data, list_data
 
 
 app = Flask("news-room-api")
+CORS(app)
+
+
+@app.before_request
+def handle_preflight():
+    if request.method == "OPTIONS":
+        res = Response()
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        res.headers.add('Access-Control-Allow-Headers', '*')
+        return res
 
 
 @app.route('/list_data', methods = ['GET'])
@@ -16,6 +27,7 @@ def list_data_():
 @app.route('/get_data', methods = ['POST'])
 def get_data_():
     return get_data(json.loads(request.get_data()))
+
 
 if __name__ == "__main__":
     app.run()
